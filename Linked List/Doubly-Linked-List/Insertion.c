ef9330135ccf15c36660e16b114f699a367e5e90
ankeshpatra <ankeshpatra.2005@gmail.com>
2024-11-08 10:13:08 +0530

    Modified the file

--- Linked List/Doubly-Linked-List/Insertion.c
+++ Linked List/Doubly-Linked-List/Insertion.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct node
+{
     int data;
     struct node *next;
     struct node *prev;
@@ -9,84 +10,125 @@ typedef struct node {
 
 node *head, *newnode, *temp;
 
-void create(int data) {
+void create(int data)
+{
     newnode = (node *)malloc(sizeof(node));
     newnode->data = data;
     newnode->next = NULL;
     newnode->prev = NULL;
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = temp = newnode;
-    } else {
+    }
+    else
+    {
         temp->next = newnode;
         newnode->prev = temp;
         temp = newnode;
     }
 }
 
-void display() {
+void display()
+{
     temp = head;
     printf("The linked list is: ");
-    while (temp != NULL) {
+    while (temp != NULL)
+    {
         printf("%d,", temp->data);
         temp = temp->next;
     }
     printf("\n");
 }
 
-void addatbeg() {
-    int data;
-    printf("Enter the data you want to insert: ");
-    scanf("%d", &data);
-    newnode = (node *)malloc(sizeof(node));
-    newnode->data = data;
-    newnode->next = head;
-    newnode->prev = NULL;
-    head->prev = newnode;
-    head = newnode;
+void addatbeg()
+{
+    if (head == NULL)
+    {
+        int data;
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        create(data);
+    }
+    else
+    {
+        int data;
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        newnode = (node *)malloc(sizeof(node));
+        newnode->data = data;
+        newnode->next = head;
+        newnode->prev = NULL;
+        head->prev = newnode;
+        head = newnode;
+    }
 }
 
-void addatend() {
-    int data;
-    newnode = (node *)malloc(sizeof(node));
-    printf("Enter the data you want to insert: ");
-    scanf("%d", &data);
-    newnode->data = data;
-    newnode->next = NULL;
-    temp = head;
-    while (temp->next != NULL) {
-        temp = temp->next;
+void addatend()
+{
+    if (head == NULL)
+    {
+        int data;
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        create(data);
     }
-    temp->next = newnode;
-    newnode->prev = temp;
+    else
+    {
+        int data;
+        newnode = (node *)malloc(sizeof(node));
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        newnode->data = data;
+        newnode->next = NULL;
+        temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->prev = temp;
+    }
 }
 
 void addatpos()
 {
-    int data,pos,i=1;
-    printf("Enter the position you want to insert: ");
-    scanf("%d",&pos);
-    printf("Enter the data you want to insert: ");
-    scanf("%d",&data);
-    newnode=(node*)malloc(sizeof(node));
-    newnode->data=data;
-    temp=head;
-    while(i<pos-1)
+    if (head == NULL)
     {
-        temp=temp->next;
-        i++;
+        int data;
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        create(data);
     }
-    newnode->next=temp->next;
-    temp->next->prev=newnode;
-    temp->next=newnode;
-    newnode->prev=temp;
+    else
+    {
+        int data, pos, i = 1;
+        printf("Enter the position you want to insert: ");
+        scanf("%d", &pos);
+        printf("Enter the data you want to insert: ");
+        scanf("%d", &data);
+        newnode = (node *)malloc(sizeof(node));
+        newnode->data = data;
+        temp = head;
+        while (i < pos - 1)
+        {
+            temp = temp->next;
+            i++;
+        }
+        newnode->next = temp->next;
+        temp->next->prev = newnode;
+        temp->next = newnode;
+        newnode->prev = temp;
+    }
 }
 
-int main() {
+int main()
+{
     head = NULL;
     int n, data;
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("Enter the data for %d node: ", i + 1);
         scanf("%d", &data);
         create(data);
